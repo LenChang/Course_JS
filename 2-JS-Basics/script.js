dc79e83d0d45e963961f3d4b294c26d02e08ff05
Len Chang <lamgo.chang@gmail.com>
2017-04-06 16:25:27 +0800

    type Coercion and Variables

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,41 @@
-console.log('Hello World by script.js')
+// Lecture: variables
+/*
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(26);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// Lecture: variables 2
+var name = 'John';
+var age = 26;
+// type Coercion 
+console.log(name + age);
+console.log(age + age)
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + 'is a' + age + 'years old' + job + '. is he married?' 
++ isMarried);
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(age + job);
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
+alert('The lastName is ' + lastName);
