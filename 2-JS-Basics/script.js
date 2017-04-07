dc04412bd089363fa84fe1babd31ba93f5b45c55
Len Chang <lamgo.chang@gmail.com>
2017-04-07 12:48:21 +0800

    Lectures Array

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -182,7 +182,7 @@ yearsUntilRetirement('Mary', 1948);
 
 /////////////////////////////////////////////////////
 // Lecture: Statements and expressions
-
+/*
 function someFun(par){
     //code
 }
@@ -199,3 +199,30 @@ var x = 3;
 if (x===5){
     //do something
 }
+*/
+
+//////////////////////////////////////////////////////
+// Lecture: Arrays
+
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names);
+console.log(names[0]);
+names[1] = 'Ben';
+console.log(names);
+
+var john = ['John','Smith',1990,'designer',false];
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+console.log(john);
+
+//alert(john.lastIndexOf('Smith'));
+
+if(john.indexOf('teacher') === -1){
+    console.log('John is NOT a teacher.');
+}
+
