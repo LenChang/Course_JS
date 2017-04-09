ca9295ac4b63c5ff5b3f3d7736290a67840094c8
Len Chang <lamgo.chang@gmail.com>
2017-04-09 13:06:25 +0800

    Lecture Loops

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -284,7 +284,7 @@ var age = john.calculateAge();
 john.age = age;
 
 console.log(john);
-*/
+
 //v2.0
 var john = {
     name: 'John',
@@ -306,3 +306,45 @@ john.yearOfBirth = 1980;
 // Don't forget to call method to change value
 john.calculateAge();
 console.log(john.age);
+*/
+
+/////////////////////////////////////////////////
+// Lecture: Loops
+
+for (var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+/*
+// for loops
+for(var i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
+
+for(var i = names.length - 1; i >= 0 ; i--){
+    console.log(names[i]);
+}
+*/
+// while loops
+var i = 0;
+while(i < names.length){
+    console.log(names[i]);
+    i++;
+}
+
+for (var i = 1; i <= 5; i++){
+    console.log(i);
+
+    if (i === 3){
+        break;
+    }
+}
+
+for (var i = 1; i <=5; i++){
+    if(i === 3){
+        continue;
+    }
+
+    console.log(i);
+}
