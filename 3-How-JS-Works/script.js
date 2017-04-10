80b67cbd143aee6e7a028e477ef5bba028b2751a
Len Chang <lamgo.chang@gmail.com>
2017-04-10 17:26:19 +0800

    Key 'this' and method borrow

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -53,7 +53,7 @@ function first() {
 
 
 // Example to show the differece between execution stack and scope chain
-
+/*
 var a = 'Hello!';
 first();
 
@@ -69,17 +69,49 @@ function first() {
 
 function third() {
     var d = 'John';
-    //console.log(c);
+    //console.log(c); // It's wrong for NotDefined Error
     console.log(a+d);
 }
-
+*/
 
 
 ///////////////////////////////////////
-// Lecture: The this keyword
+// Lecture: The this keyword and Method borrow.
+/*
+console.log(this);
 
+calculateAge(1990);
+function calculateAge(year){
+    console.log(2016 - year);
+    console.log(this);
+}
+*/
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function () {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
 
+        
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        
+    }
+};
+
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+// method borrowing.
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
 
 
